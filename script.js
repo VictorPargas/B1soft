2099a87aaf5f9faf403e84a93890998a18d823f3
Victor <victorparga765@gmail.com>
2023-04-03 14:29:44 -0300

    :tada:primeiro commit

---
+++ script.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.main', {duration: 1000});
+
+sr.reveal('.sobre', {
+  rotate: { x:0, y: 80, z: 0},
+  duration: 2000
+});
+
+sr.reveal('.conquistas' ,{
+duration:2000,
+});
+
